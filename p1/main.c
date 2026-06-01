6984ab273b89497e075a3bae892bdef107ba3529
2025020068 <bangyunjae5354@gmail.com>
2026-06-01 16:13:45 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,17 +3,16 @@
 int main(void)
 {
     char arr[10]={};
-    char max_ch;
+    char max_ch= '\0';
     char *p;
     char *q;
-    int count;
     int max_count=0;
     for(p=arr;p<arr+10;p++){
     scanf("%c",p);
     }
     
     for (p=arr;p<arr+10;p++){
-        count=0;
+        int count=0;
         for (q=arr;q<arr+10;q++){
             if(*p==*q) 
             count++;
